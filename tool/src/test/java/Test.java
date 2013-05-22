758ba1cdc5f991be186ea1983f2e0ec89b7af850
Leandro <leandro@cheetah.(none)>
2013-05-22 19:42:36 -0400

    Edited some tooltip messages

--- tool/src/test/java/Test.java
+++ tool/src/test/java/Test.java
@@ -6,11 +6,11 @@ import org.testevol.domain.Version;
 
 public class Test {
 
-	//@org.junit.Test
+	@org.junit.Test
 	public void test() {
 		try {
-			Version version = new Version(new File("/home/leandro/Documents/Atlanta/workspace/testevol-web/testevol-web/tool/tmp/projects/Gson/v.1.0"));
-			version.setUp(new File("/home/leandro/Documents/Atlanta/workspace/testevol-web/testevol-web/tool/config"));
+			Version version = new Version(new File("/home/leandro/TestEvol/tool/tmp/projects/Google-gson/v.1.0/"));
+			version.setUp(new File("/home/leandro/TestEvol/tool/config/"));
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
